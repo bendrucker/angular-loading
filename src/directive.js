2540885f0f147e2a6e7be2a9c2a4b7cc8de04a0b
Ben Drucker <bvdrucker@gmail.com>
2014-11-07 14:17:59 -0500

    Use partialify

--- src/directive.js
+++ src/directive.js
@@ -1,14 +1,12 @@
 'use strict';
 
-var fs = require('fs');
-
 module.exports = function () {
   return {
     scope: {
       loading: '='
     },
     transclude: true,
-    template: fs.readFileSync('./template.html') + fs.readFileSync('./transclude.html'),
+    template: require('./template.html') + require('./transclude.html'),
     link: function (scope) {
       scope.loading.then(function () {
         scope.loaded = true
